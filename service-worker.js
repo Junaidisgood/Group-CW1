81ceb6abec8493fb3f5ea1f24520f82047872368
Junaid Bashir <junaid@crayondevelopers.com>
2021-04-01 11:02:32 +0400

    donload cacheFiles to cache storage

--- service-worker.js
+++ service-worker.js
@@ -6,3 +6,13 @@ var cacheFiles = [
     'english.jpg',
     'icon-512.png'
 ];
+
+// download cacheFiles to cache
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            console.log('[Service Worker] Caching all the files');
+            return cache.addAll(cacheFiles);
+        }));
+});
